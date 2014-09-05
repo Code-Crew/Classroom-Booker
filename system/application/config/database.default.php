e6bb939787ff6ad20498dead5f86c6462f3a7ee9
Jack <jack@code-crew.net>
2014-09-05 02:42:24 -0400

    first commit

---
+++ system/application/config/database.default.php
@@ -0,0 +1,17 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$active_group = "default";
+
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'crbs1';
+$db['default']['dbdriver'] = 'mysqli';
+$db['default']['dbprefix'] = '';
+$db['default']['active_r'] = TRUE;
+$db['default']['pconnect'] = FALSE;
+$db['default']['db_debug'] = FALSE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = 'dbcache';
+
+?>
