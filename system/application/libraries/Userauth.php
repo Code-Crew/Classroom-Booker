2b1d5026feab57127dbaab59a1fdd896d4c23e49
JK <jack@code-crew.net>
2014-09-11 16:08:47 -0400

    LDAP Account Login

--- system/application/libraries/Userauth.php
+++ system/application/libraries/Userauth.php
@@ -58,10 +58,12 @@ class Userauth{
 		if($ldap == NULL) { $ldap = $this->ldap_info; }
 		$buffer = array();
 		foreach($ldap as $key => $value) {
+			var_dump($value);
 			if(is_array($value)) {
 				$ldap[$key] = $value[0];
 			}
 		}
+		die();
 		return $buffer;
 	}
 	
