f45763347536f4d860c50a7e4a943608946056a6
JK <jack@code-crew.net>
2014-09-11 16:28:12 -0400

    LDAP Account Login

--- system/application/libraries/Userauth.php
+++ system/application/libraries/Userauth.php
@@ -181,6 +181,7 @@ class Userauth{
 	function tryassign($username, $password, $ldap) {
 		if( $username == '' && $password == '') { return false; }
 		var_dump($username);
+		var_dump($password);
 		if(!is_array($ldap)) { $ldap = json_decode($ldap, true); }
 		if($ldap === NULL) { return false; }
 		var_dump($ldap);
